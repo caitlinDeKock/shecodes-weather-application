b169899b51f3667c98552e5080228f5b8ade11be
caitlinDeKock <163155850+caitlinDeKock@users.noreply.github.com>
2024-03-24 12:37:01 +0200

    Added search functionality using weather API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let cityElement = document.querySelector("#city");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "fdtba1b75abd823874fca8d73007o460";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Cape Town");
